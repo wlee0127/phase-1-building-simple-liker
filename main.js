fe8730ab43f69ee52b6c25f04b68e2ef2d160f83
william lee <will.lee.npc@gmail.com>
2023-10-14 21:27:32 -0400

    complete lab

--- main.js
+++ main.js
@@ -3,9 +3,56 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.getElementById("modal").hidden = true;
 
+let heartClass = document.getElementsByClassName("like-glyph");
+let errorModal = document.getElementById("modal");
+console.log(heartClass);
 
+for (const element of heartClass) {
+  
+  element.addEventListener("click", function(event){
+    mimicServerCall()
+    .then(function (response){
+      console.log(response);
+      let elementSpan = event.target;
+      
+      if(element.classList.contains("activated-heart")){
+        elementSpan.innerText = EMPTY_HEART;
+        elementSpan.removeAttribute("class", "activated-heart");
+      }else{
+        elementSpan.innerText = FULL_HEART;
+        elementSpan.setAttribute("class", "activated-heart");
+      }
+      return response;
+    })
+    .catch(function (error){
+      console.log(error);
+      document.getElementById("modal").hidden = false;
+      errorModal.innerText = error;
+      setTimeout(() => {document.getElementById("modal").hidden = true}, 3000);
+    })
+  });
+}
 
+/*
+function myFunction(event){
+  mimicServerCall()
+    .then(function (response){
+      console.log(response)
+      let a = event.target;
+      console.log(a)
+      return response;
+    })
+    .catch(function (error){
+      console.log(error);
+      document.getElementById("modal").hidden = false;
+      errorModal.innerText = error;
+      setTimeout(() => {document.getElementById("modal").hidden = true}, 3000);
+    })
+    //element.innerText=FULL_HEART;
+  }
+*/
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
